bd3eae32e55990a55b2c3aebd53dfc97e9e5a6e1
Cristian Incarnato <cristian.cdi@gmail.com>
2022-02-09 11:14:40 -0300

    fix route crud

--- src/generator-front/routes/IndexRoute.js
+++ src/generator-front/routes/IndexRoute.js
@@ -1,9 +1,9 @@
 module.exports = function (moduleName) {
     let content =
 `import merge from 'deepmerge'
-import ${moduleName}ManagementRoutes from './${moduleName}ManagementRoutes'
+import ${moduleName}CrudRoutes from './${moduleName}CrudRoutes'
 
-const routes = merge.all([${moduleName}ManagementRoutes])
+const routes = merge.all([${moduleName}CrudRoutes])
 
 
 export default routes;
